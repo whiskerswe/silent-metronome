76ce2a5e42125ce3c2851fa85f78f228da9f20f5
Sara Wänerskär <sara.wanerskar@gmail.com>
2026-09-24 19:11:14 +0200

    More pwa fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,41 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/silent-metronome/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Silent Metronome',
+        short_name: 'Metronome',
+        description: 'A silent visual metronome',
+        start_url: '/silent-metronome/',
+        scope: '/silent-metronome/',
+        display: 'standalone',
+        theme_color: '#1c1713',
+        background_color: '#1c1713',
+        icons: [
+          {
+            src: 'icons/lyre192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/lyre512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/lyremascable512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
